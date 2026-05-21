10dbfccd6a2a29b6168358628c32db29fd6a5876
star <itcoder007@126.com>
2026-05-21 08:49:18 +0800

    dev: iteration 2 - restructure Hello.java to mirror Python main() pattern, extract run() method, 15/15 tests passed

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
-    public static void main(String[] args) {
+
+    private static void run() {
         System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        run();
     }
 }
